784d1c183742f8ed1a4e8c769a65ceccbe3c1666
AdmiN <AdmiN@ITWS05>
2023-09-21 14:45:37 +0530

    ok

--- src/com/lbrce/cseasec/Sample.java
+++ src/com/lbrce/cseasec/Sample.java
@@ -4,7 +4,14 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+naveen();
 	}
+
+
+public static void naveen() {
+	// TODO Auto-generated method stub
+	System.out.println("this is team member");
+
+}
 
 }
